5e8fea1f8c0309e6906bd344273042029ff5c662
NethmiDivyaT <68665603+NethmiDivyaT@users.noreply.github.com>
2022-02-28 16:42:06 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import './App.css';
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
 import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 
 
@@ -26,21 +20,23 @@ function App() {
         <NavDropdown.Divider />
         <NavDropdown.Item className="Navitem" href="#">Premium Plan</NavDropdown.Item>
         </NavDropdown>
-        </Nav>
-         { /**Search Bar */}
-    <form class="d-flex input-group w-auto">
-        <input
-        className='searchI'
-        type="search"
-        class="form-control rounded"
-        placeholder="Search"
-        aria-label="Search"
-        aria-describedby="search-addon"
-      />
-      <span class="input-group-text border-0" id="search-addon">
-        <i class="fa fa-search"></i>
-            </span>
-            </form>
+      </Nav>
+      <Nav>
+            { /**Search Bar */}
+            <form class="d-flex input-group w-auto">
+            <input
+            className='searchI'
+            type="search"
+            class="form-control rounded"
+            placeholder="Search"
+            aria-label="Search"
+            aria-describedby="search-addon"
+          />
+          <span class="input-group-text border-0" id="search-addon">
+          <i class="fa fa-search"></i>
+          </span>
+          </form>
+      </Nav>
   </Navbar.Collapse>
   </Container>
     </Navbar>
