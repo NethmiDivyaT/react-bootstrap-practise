4fc45dbb16df6d4d27e40f7ebe02b8a7711a0b32
NethmiDivyaT <68665603+NethmiDivyaT@users.noreply.github.com>
2022-03-07 01:35:24 +0530

    update card

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -1,59 +1,125 @@
 import '../Styles/Card.css';
 import { Card, Col, Container, Row } from 'react-bootstrap';
-import img1 from "../Assert/1.jpg";
-import img2 from "../Assert/4.jpg";
-import img3 from "../Assert/bg.jpg";
-import img4 from "../Assert/about1.jpg";
+import 'font-awesome/css/font-awesome.min.css';
+import img1 from "../Assert/img1.jpeg";
+import img2 from "../Assert/img2.jpg";
+import img3 from "../Assert/img3.jpg";
+import img4 from "../Assert/img4.jpg";
 
 function CardView() {
     return (
         <Container className='Container'>
         <Row xs={1} md={4}>
           <Col>
-            <Card className='card1'>
+            <Card className='card'>
               <Card.Img variant="top" src={img1} className="img" />
               <Card.Body>
-                <Card.Title className='title'>Card title</Card.Title>
-                <Card.Text>
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit longer.
+              <button>$15</button>
+                <Card.Title className='title'>Programming with HTML,CSS and JS</Card.Title>
+                <Card.Text className="content">
+                 Learn basic web development as you build web pages.
                 </Card.Text>
+                <div>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star-half-o" aria-hidden="true"></i>
+                  <i class="fa fa-star-o" aria-hidden="true"></i>
+                  <i className='count'>4.8</i>
+                </div>
+                <div className='footergroup'>
+                  <i class="fa fa-youtube-play" aria-hidden="true"></i>
+                  <i className='iconName'>Video</i>
+                  <i class="fa fa-video-camera" aria-hidden="true"></i>
+                  <i className='iconName'>54</i>
+                  <i class="fa fa-download" aria-hidden="true"></i>
+                  <i className='iconName'>54</i>
+                </div>
+
               </Card.Body>
             </Card>
             </Col>
             <Col>
-            <Card className='card2'>
+            <Card className='card'>
               <Card.Img variant="top" src={img2} className="img" />
               <Card.Body>
-                <Card.Title className='title'>Card title</Card.Title>
-                <Card.Text>
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit longer.
+              <button>$15</button>
+                <Card.Title className='title'>Modern Cloud Based Application Development</Card.Title>
+                <Card.Text className="content">
+                  Start buildning of modern software application in the cloud.
                 </Card.Text>
+                <span>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star-half-o" aria-hidden="true"></i>
+                  <i class="fa fa-star-o" aria-hidden="true"></i>
+                  <i className='count'>4.8</i>
+                </span>
+                <div className='footergroup'>
+                  <i class="fa fa-youtube-play" aria-hidden="true"></i>
+                  <i className='iconName'>Video</i>
+                  <i class="fa fa-video-camera" aria-hidden="true"></i>
+                  <i className='iconName'>54</i>
+                  <i class="fa fa-download" aria-hidden="true"></i>
+                  <i className='iconName'>54</i>
+                </div>
               </Card.Body>
             </Card>
             </Col>
             <Col>
-            <Card className='card3'>
+            <Card className='card'>
               <Card.Img variant="top" src={img3} className="img" />
               <Card.Body>
-                <Card.Title className='title'>Card title</Card.Title>
-                <Card.Text>
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit longer.
+              <button>$15</button>
+                <Card.Title className='title'>Full Stack web application Development</Card.Title>
+                <Card.Text className="content">
+                  Learn concepts to build modern full stack web applications.
                 </Card.Text>
+                <span>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star-half-o" aria-hidden="true"></i>
+                  <i class="fa fa-star-o" aria-hidden="true"></i>
+                  <i className='count'>4.8</i>
+                </span>
+                <div className='footergroup'>
+                  <i class="fa fa-youtube-play" aria-hidden="true"></i>
+                  <i className='iconName'>Video</i>
+                  <i class="fa fa-video-camera" aria-hidden="true"></i>
+                  <i className='iconName'>54</i>
+                  <i class="fa fa-download" aria-hidden="true"></i>
+                  <i className='iconName'>54</i>
+                </div>
               </Card.Body>
             </Card>
             </Col>
             <Col>
-            <Card className='card4'>
+            <Card className='card'>
               <Card.Img variant="top" src={img4} className="img" />
               <Card.Body>
-                <Card.Title className='title'>Card title</Card.Title>
-                <Card.Text>
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit longer.
+              <button>$15</button>
+                <Card.Title className='title'>Computer Networking for Software Engineers</Card.Title>
+                <Card.Text className="content">
+                  Fundermentals of software applications in the cloud.
                 </Card.Text>
+                <span>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star-half-o" aria-hidden="true"></i>
+                  <i class="fa fa-star-o" aria-hidden="true"></i>
+                  <i className='count'>4.8</i>
+                </span>
+                <div className='footergroup'>
+                  <i class="fa fa-youtube-play" aria-hidden="true"></i>
+                  <i className='iconName'>Video</i>
+                  <i class="fa fa-video-camera" aria-hidden="true"></i>
+                  <i className='iconName'>54</i>
+                  <i class="fa fa-download" aria-hidden="true"></i>
+                  <i className='iconName'>54</i>
+                </div>
               </Card.Body>
             </Card>
           </Col>
