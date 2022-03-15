377aa194fe3227997d5ea58c548c5071d6265599
NethmiDivyaT <68665603+NethmiDivyaT@users.noreply.github.com>
2022-03-15 19:15:40 +0530

    typehead responsive

--- src/Component/typehead.js
+++ src/Component/typehead.js
@@ -7,20 +7,24 @@ import { Typeahead, ClearButton } from 'react-bootstrap-typeahead';
 import options from './data';
 
 const BasicExample = () => {
-    return (
-      <Container className='container'>
-              <Typeahead
-                id="service"
-                options={options}
-                placeholder="Choose a Language........">
-                {({ onClear, selected }) => (
-                        <div className="rbt-aux">
-                            {!!selected.length && <ClearButton onClick={onClear} size="lg" className='clear'/>}
-                            {!selected.length && <Spinner animation="grow" size="sm" />}
-                        </div>
-                        )}
-               </Typeahead>
-        </Container>
+  return (
+      <div className='bg'>
+      <Form.Group className='container'>
+        <Typeahead
+          className='typehead'
+          id="basic-typeahead-single"
+          options={options}
+          placeholder="Choose a Language........">
+          {({ onClear, selected }) => (
+                  <div className="rbt-aux">
+                      {!!selected.length && <ClearButton onClick={onClear} size="lg" className='clear'/>}
+                      {!selected.length && <Spinner animation="grow" size="sm" />}
+                  </div>
+          )}
+          </Typeahead>
+      </Form.Group>
+      </div>
+      
     );
   };
 
